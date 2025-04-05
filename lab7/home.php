4b0ea77582a2b8c21670c2e3390b023c5c362b23
Eeeeast <107624551+Eeeeast@users.noreply.github.com>
2025-04-05 16:29:59 +0300

    as

--- lab7/home.php
+++ lab7/home.php
@@ -25,27 +25,9 @@ $posts = json_decode($usersPosts, true);
     </head>
     <body>
         <?php foreach ($posts as $post) {
-            if (!$filter | ($post["user-id"] === $filter)) { ?>
-            <div>
-                <div>
-                    <img src="<?= findUserById($users, $post["user-id"])["ava"][
-                        "src"
-                    ] ?>" alt="User Avatar" width="30" />
-                </div>
-                <div>
-                    <img src="<?= $post[
-                        "image"
-                    ] ?>"alt="post image" width="300" />
-                    <div>
-                        <button type="button">❤️<?= $post[
-                            "like-count"
-                        ] ?></button>
-                    </div>
-                    <p><?= $post["text"] ?></p>
-                    <p><?= $post["time"] ?></p>
-                </div>
-            </div>
-        <?php }
+            if (!$filter | ($post["user-id"] === $filter)) {
+                include "post-template.php";
+            }
         } ?>
     </body>
 </html>
---
+++ lab7/post-template.php
@@ -0,0 +1,15 @@
+<div>
+    <div>
+        <img src="<?= findUserById($users, $post["user-id"])["ava"][
+            "src"
+        ] ?>" alt="User Avatar" width="30" />
+    </div>
+    <div>
+        <img src="<?= $post["image"] ?>"alt="post image" width="300" />
+        <div>
+            <button type="button">❤️<?= $post["like-count"] ?></button>
+        </div>
+        <p><?= $post["text"] ?></p>
+        <p><?= $post["time"] ?></p>
+    </div>
+</div>
